c95df3debb564f087103544e1e195146492f63dd
Nadelio <carefulsniffle@gmail.com>
2024-10-02 17:11:47 -0500

    Minor dev-side refactoring

--- eBF External Programming/src/eBF/eBFInterpreter.java
+++ eBF External Programming/src/eBF/eBFInterpreter.java
@@ -11,16 +11,15 @@ public class eBFInterpreter {
     private static DoubleByte[] stack = new DoubleByte[256];
     private static int stackPointer = 0;
 
-    // RAM OPERATIONS
+    // Tape OPERATIONS
     private static int pointerX = 0;
     private static int pointerY = 0;
     private static int pointerValue = 0;
-    private static DoubleByte[][] RAM = new DoubleByte[256][256];
+    private static DoubleByte[][] Tape = new DoubleByte[256][256];
     private static final int maxPointerValue = (int) Math.pow(2, 8) - 1;
     
     // MISC VARS
     private static int tokenNumber = 0;
-    private static boolean simMode = true;
     private static HashMap<String, File> dependencies = new HashMap<String, File>();
     private static HashMap<String, int[]> labels = new HashMap<String, int[]>();
 
@@ -48,7 +47,7 @@ public class eBFInterpreter {
                     movePointerLeft();
                     break;
                 case "[":
-                    if(RAM[pointerX][pointerY].isZero()) {
+                    if(Tape[pointerX][pointerY].isZero()) {
                         i++;
                         while(c > 0 || !eBFTokens[i].equals("]")) {
                             
@@ -64,7 +63,7 @@ public class eBFInterpreter {
                     }
                     break;
                 case "]":
-                    if(!RAM[pointerX][pointerY].isZero()) {
+                    if(!Tape[pointerX][pointerY].isZero()) {
                         i--;
                         while(c > 0 || !eBFTokens[i].equals("[")) {
                             
@@ -80,14 +79,14 @@ public class eBFInterpreter {
                     }
                     break;
                 case ",":
-                    RAM[pointerX][pointerY] = DoubleByte.convertToDoubleByte(pointerValue);
+                    Tape[pointerX][pointerY] = DoubleByte.convertToDoubleByte(pointerValue);
                     break;
                 case "=": // write to terminal
-                    System.out.print((char) (RAM[pointerX][pointerY].convertToInt() + 32));
+                    System.out.print((char) (Tape[pointerX][pointerY].convertToInt() + 32));
                     break;
                 case ".":
-                    RAM[pointerX][pointerY].setHighByte((byte) System.in.read());
-                    RAM[pointerX][pointerY].setLowByte((byte) System.in.read());
+                    Tape[pointerX][pointerY].setHighByte((byte) System.in.read());
+                    Tape[pointerX][pointerY].setLowByte((byte) System.in.read());
                     break;
                 case ">>":
                     incrementStackPointer();
@@ -117,8 +116,8 @@ public class eBFInterpreter {
                     }
                     // else if(inSimMode()){ ePUx16Sim.runDependency(eBFTokens[i+1]); i++;}
                     break;
-                case "'": // read from RAM
-                    pointerValue = RAM[pointerX][pointerY].convertToInt();
+                case "'": // read from Tape
+                    pointerValue = Tape[pointerX][pointerY].convertToInt();
                     break;
                 case "#":
                     labels.put(eBFTokens[i+1], new int[]{ pointerX, pointerY });
@@ -165,14 +164,14 @@ public class eBFInterpreter {
                     movePointerLeft();
                     break;
                 case "0000000000000101":
-                    if (RAM[pointerX][pointerY].isZero()) {
+                    if (Tape[pointerX][pointerY].isZero()) {
                         i++;
-                        while(c > 0 || !eBinTokens[i].equals("0110")) {
-                            if(eBinTokens[i].equals("0101")) {
+                        while(c > 0 || !eBinTokens[i].equals("0000000000000110")) {
+                            if(eBinTokens[i].equals("0000000000000101")) {
                                 c++;
-                            } else if(eBinTokens[i].equals("0110")) {
+                            } else if(eBinTokens[i].equals("0000000000000110")) {
                                 c--;
-                            } else if(eBinTokens[i].equals("1011")) {
+                            } else if(eBinTokens[i].equals("0000000000001011")) {
                                 throw new Exception("Looped Dependency Set Call" + tokenNumber);  
                             }
                             i++;
@@ -180,15 +179,15 @@ public class eBFInterpreter {
                     }
                     break;
                 case "0000000000000110":
-                    if (!RAM[pointerX][pointerY].isZero()) {
+                    if (!Tape[pointerX][pointerY].isZero()) {
                         i--;
-                        while(c > 0 || !eBinTokens[i].equals("0101")) {
+                        while(c > 0 || !eBinTokens[i].equals("0000000000000101")) {
                             
-                            if(eBinTokens[i].equals("0110")) {
+                            if(eBinTokens[i].equals("0000000000000110")) {
                                 c++;
-                            } else if(eBinTokens[i].equals("0101")) {
+                            } else if(eBinTokens[i].equals("0000000000000101")) {
                                 c--;
-                            } else if(eBinTokens[i].equals("1011")) {
+                            } else if(eBinTokens[i].equals("0000000000001011")) {
                                 throw new Exception("Looped Dependency Set Call" + tokenNumber);  
                             }
                             i--;
@@ -196,14 +195,14 @@ public class eBFInterpreter {
                     }
                     break;
                 case "0000000000000111":
-                    RAM[pointerX][pointerY] = DoubleByte.convertToDoubleByte(pointerValue);
+                    Tape[pointerX][pointerY] = DoubleByte.convertToDoubleByte(pointerValue);
                     break;
-                case "0000000000001101": // read from RAM
-                    pointerValue = RAM[pointerX][pointerY].convertToInt();
+                case "0000000000001101": // read from Tape
+                    pointerValue = Tape[pointerX][pointerY].convertToInt();
                     break;
                 case "0000000000001000":
-                    RAM[pointerX][pointerY].setHighByte((byte) System.in.read());
-                    RAM[pointerX][pointerY].setLowByte((byte) System.in.read());
+                    Tape[pointerX][pointerY].setHighByte((byte) System.in.read());
+                    Tape[pointerX][pointerY].setLowByte((byte) System.in.read());
                     break;
                 case "0000000000001001":
                     incrementStackPointer();
@@ -232,7 +231,7 @@ public class eBFInterpreter {
                     }
                     break;
                 case "0000000000001110": // write to terminal
-                    System.out.print((char) (RAM[pointerX][pointerY].convertToInt() + 32));
+                    System.out.print((char) (Tape[pointerX][pointerY].convertToInt() + 32));
                     break;
                 case "0000000000010001":
                     labels.put(eBinTokens[i+1], new int[]{ pointerX, pointerY });
@@ -258,7 +257,7 @@ public class eBFInterpreter {
     private static boolean DEBUG_FLAG = false;
 
     public static void main(String[] args){
-        initializeRAM();
+        initializeTape();
         initializeStack();
 
         if(args.length == 0){
@@ -290,10 +289,10 @@ public class eBFInterpreter {
         simMode = false;
     }
 
-    private static void initializeRAM(){
+    private static void initializeTape(){
         for(int i = 0; i < 255; i++){
             for(int j = 0; j < 255; j++){
-                RAM[i][j] = new DoubleByte((byte) 0x0000, (byte) 0x0000);
+                Tape[i][j] = new DoubleByte((byte) 0x0000, (byte) 0x0000);
             }
         }
     }
@@ -310,9 +309,9 @@ public class eBFInterpreter {
             System.out.println("Token: " + tokenNumber + " ( " + token + " )");
 
             // Tape
-            for(int i = 0; i < RAM.length; i++){
-                for(int j = 0; j < RAM[i].length; j++){
-                    System.out.print("[" + RAM[i][j].convertToInt() + "]");
+            for(int i = 0; i < Tape.length; i++){
+                for(int j = 0; j < Tape[i].length; j++){
+                    System.out.print("[" + Tape[i][j].convertToInt() + "]");
                 }
             }
             System.out.println();
